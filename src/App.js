8100df70075b179d66316b2e55a943b40d239e36
Gug4ZL <gustavo2004ct@gmail.com>
2024-03-24 23:43:28 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import People from "./assets-react/people.svg";
+import Arrow from "./assets-react/arrow-rigth.svg";
+import Trash from "./assets-react/trash.svg";
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button, User } from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+    const [users, setUsers] = useState([]);
+    const inputName = useRef();
+    const inputAge = useRef();
+
+    useEffect(() => {
+
+        async function FetchUsers() {
+
+            const { data: newUsers } = await axios.get("http://localhost:3001/users");
+
+            setUsers(newUsers);
+        }
+
+        FetchUsers()
+
+    }, [])
+
+    async function addNewUser() {
+
+        const { data: newUser } = await axios.post("http://localhost:3001/users", {
+            name: inputName.current.value,
+            age: inputAge.current.value,
+        });
+
+
+
+        setUsers([
+            ...users, newUser
+
+        ]);
+
+
+
+    }
+
+    async function deleteUSer(userId) {
+        await axios.delete(`http://localhost:3001/users/${userId}`);
+
+        const newUsers = users.filter(user => user.id !== userId);
+
+        setUsers(newUsers);
+    }
+
+
+
+    return (
+        <Container>
+            <Image alt="logo-imagem" src={People} />
+            <ContainerItens>
+                <H1>Ola!</H1>
+
+                <InputLabel>Nome</InputLabel>
+                <Input ref={inputName}></Input>
+
+                <InputLabel>Idade</InputLabel>
+                <Input ref={inputAge} ></Input>
+
+                <Button onClick={addNewUser}>Cadatrar<img alt="seta" src={Arrow}></img></Button>
+
+                <ul>
+                    {users.map((user) => (
+                        <User key={user.id}>
+                            <p>{user.name}</p><p>{user.age}</p>
+
+                            <button onClick={() => deleteUSer(user.id)}>
+                                <img src={Trash} alt="Lata-de-lixo" />
+                            </button>
+                        </User>
+                    ))}
+                </ul>
+
+            </ContainerItens>
+
+        </Container>
+    );
 }
 
-export default App;
+export default App
---
+++ src/styles.js
@@ -0,0 +1,136 @@
+import styled from 'styled-components';
+import Background from './assets-react/background1.svg'
+
+
+export const Container = styled.div`
+    background: url("${Background}");
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+ 
+`
+export const Image = styled.img`
+margin-top: 30px;
+`;
+
+export const ContainerItens = styled.div`
+background: linear-gradient(157.44deg, rgba(255, 255, 255, 0.6) 0.84%, rgba(255, 255, 255, 0.6) 0.85%, rgba(255, 255, 255, 0.15) 100%);
+border-radius: 61px 61px 0px 0px;
+
+padding: 50px 36px;
+display: flex;
+flex-direction: column;
+
+
+`;
+
+
+
+
+export const H1 = styled.h1`
+font-size: 34px;
+font-weight: 700;
+line-height: 40px;
+text-align: center;
+color: #EEEEEE;
+
+margin-bottom: 80px;
+`;
+
+export const InputLabel = styled.p`
+font-size: 18px;
+font-weight: 700;
+line-height: 22px;
+text-align: left;
+color: #EEEEEE;
+
+margin-left: 10px;
+
+`;
+
+export const Input = styled.input`
+width: 342px;
+height: 58px;
+padding: 15px 20px 15px 25px;
+border-radius: 14px;   
+margin-bottom: 34px;
+
+background: #FFFFFF40;
+box-shadow: 0px 4px 4px 0px #00000040;
+
+border: none;
+outline: none;
+
+`;
+
+export const Button = styled.button`
+width: 342px;
+height: 74px;
+border-radius: 14px;
+display:flex;
+align-items: center;
+justify-content: center;
+gap: 20px;
+
+
+background: rgba(0, 0, 0, 0.8);
+border:none;
+
+
+font-size: 17px;
+font-weight: 700;
+line-height: 28px;
+color: rgba(255, 255, 255, 1);
+
+cursor: pointer;
+
+&:hover{
+    opacity:0.8
+}
+
+&:active{
+    opacity:0.5
+}
+`;
+
+export const User = styled.li`
+display: flex;
+justify-content: space-around;
+align-items: center;
+margin-top: 20px;
+
+width: 342px;
+height: 58px;
+padding: 15px 20px 15px 25px;
+border-radius: 14px;   
+margin-bottom: 34px;
+
+background: #FFFFFF40;
+box-shadow: 0px 4px 4px 0px #00000040;
+
+border: none;
+outline: none;
+
+p{
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 28px;
+
+    color: #ffffff;
+}
+
+button{
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+`
+
+
+
+
+
